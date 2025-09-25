bc962e25204f90dd154b85d5a457ad53075b35a1
David Nurdin <david.nurdin@respawnsive.com>
2025-09-25 15:56:50 +0200

    update

--- compile.sh
+++ compile.sh
@@ -1,15 +1,18 @@
 ./clean.sh
 ./create.sh
 git add . && git commit -m 'update' && git push
-
 sudo rm -rf $(go env GOPATH)/pkg/mod/cache/ ~/.xcaddy
 sudo rm -rf $(go env GOPATH)/pkg/mod/github.com/ ~/.xcaddy
+
+
+rm ./myfranken
+
 CGO_ENABLED=1 \
 XCADDY_GO_BUILD_FLAGS="-ldflags='-w -s' -tags=nobadger,nomysql,nopgx" \
 CGO_CFLAGS=$(php-config --includes) \
 CGO_LDFLAGS="$(php-config --ldflags) $(php-config --libs)" \
 xcaddy build \
-  --output /home/david/poc/my_extension/my_extension/frankenphp \
+  --output /home/david/poc/my_extension/my_extension/myfranken \
   --with github.com/dunglas/frankenphp/caddy \
   --with github.com/dunglas/mercure/caddy \
   --with github.com/dunglas/vulcain/caddy \
