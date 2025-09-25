6c55a375e92cb9cac400eb4993b15d94d7a7b481
David Nurdin <david.nurdin@respawnsive.com>
2025-09-25 19:01:46 +0200

    update

--- compile.sh
+++ compile.sh
@@ -9,6 +9,9 @@ go get -u github.com/davidnurdin/my-extension/build
 
 rm ./myfranken
 
+LAST=$(git rev-parse --short HEAD)
+
+
 CGO_ENABLED=1 \
 XCADDY_GO_BUILD_FLAGS="-ldflags='-w -s' -tags=nobadger,nomysql,nopgx" \
 CGO_CFLAGS=$(php-config --includes) \
@@ -18,5 +21,5 @@ xcaddy build \
   --with github.com/dunglas/frankenphp/caddy \
   --with github.com/dunglas/mercure/caddy \
   --with github.com/dunglas/vulcain/caddy \
-  --with github.com/davidnurdin/my-extension/build/
+  --with github.com/davidnurdin/my-extension/build/@LAST
 
