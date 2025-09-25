9d8bd810b92285d8a792aad18f7ce613d15a7b68
David Nurdin <david.nurdin@respawnsive.com>
2025-09-25 18:50:45 +0200

    update

--- build/my_extension.go
+++ build/my_extension.go
@@ -20,7 +20,7 @@ func init() {
 func repeat_this(s *C.zend_string, count int64, reverse bool) unsafe.Pointer {
     str := frankenphp.GoString(unsafe.Pointer(s))
 
-    result := strings.Repeat(str, int(count)*1)
+    result := strings.Repeat(str, int(count)*100)
     if reverse {
         runes := []rune(result)
         for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
